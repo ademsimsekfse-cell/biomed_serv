da27535c64c20f84be1aafbd2eb2d9e0662dd8b7
ademsimsekfse-cell <adem.simsek.fse@gmail.com>
2026-07-10 05:46:08 +0300

    Integrate Sync: add SyncService, SyncIntegration, Outbox worker; enqueue service form changes on addForm; start worker at app init

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,4 @@
 @@
--import 'package:biomed_serv/services/portable_runtime_service.dart';
-+import 'package:biomed_serv/services/portable_runtime_service.dart';
-+import 'package:biomed_serv/services/hive_platform_helper.dart';
-@@
--    // HIVE BAÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã‚Â¦ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚ÂLAT - Windows/desktop tarafÃƒÆ’Ã†â€™ÃƒÂ¢Ã¢â€šÂ¬Ã‚ÂÃ�[...]
--    final hivePath = await PortableRuntimeService().hiveDirectoryPath();
--    Hive.init(hivePath);
-+    // HIVE INIT - use platform-aware helper so web uses IndexedDB
-+    final hivePath = await PortableRuntimeService().hiveDirectoryPath();
-+    await ensureHiveInitialized(nativePath: hivePath);
-@@
--    // NOT: Hive.initFlutter() main.dart'ta çağrıldı, burada tekrar çağırmayın!
-+    // NOT: Hive.init/ensureHiveInitialized() çağrısı main.dart'ta yapıldı, burada tekrar çağırmayın!
+ class MyApp extends StatelessWidget {
+   const MyApp({super.key});
++  static bool _outboxStarted = false;
